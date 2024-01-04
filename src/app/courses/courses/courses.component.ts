b432fac331f44c3eed089f0b7e78d2b88d9aeb15
Matheus Miranda Luchiari <mm.luchiari2002@gmail.com>
2024-01-03 22:11:46 -0300

    Commit Inicial

---
+++ src/app/courses/courses/courses.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { Course } from '../model/course';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrl: './courses.component.scss'
+})
+export class CoursesComponent {
+
+  courses: Course[] = [
+    { _id: '1', name: 'Angular', category: 'Front-end' }
+  ];
+  displayedColumns: string[] = ['name', 'category'];
+
+  constructor() {}
+
+  ngOnInit(): void {
+
+  }
+
+
+}
